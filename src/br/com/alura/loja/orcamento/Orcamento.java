645cd5092f14a6d56f224d1689749da2bc876d39
tonhaoSfalcini <antoniosfalcini@gmail.com>
2021-06-02 15:03:36 -0300

    Novo cenário. Mudança de situação do Orçamento e novo desconto

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -10,4 +10,22 @@ public class Orcamento {
 
 	private BigDecimal valor;
 	private Integer qtdeItens;
+	private String situacao;
+	
+	
+	public void aplicaDescontoExtra() {
+		if(situacao.equals("ANALISE")) {
+			this.valor = this.valor.subtract(this.valor.multiply(new BigDecimal("0.05")));
+		}else if(situacao.equals("APROVADO")) {
+			this.valor = this.valor.subtract(this.valor.multiply(new BigDecimal("0.02")));
+		}
+	}
+	
+	public void aprovar() {
+		this.situacao = "APROVADO";
+	}
+
+	public void iniciarAnalise() {
+		this.situacao = "ANALISE";
+	}
 }
